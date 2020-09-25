82d509fc11f7b1a6a8776a186cd6e7a9bbe44183
YongTat <tanyongtat.tyt@gmail.com>
2020-09-25 15:53:54 +0800

    added notes

--- Week 3/question2.c
+++ Week 3/question2.c
@@ -4,9 +4,9 @@ float a,b,c,d,e;
 
 int main(){
     a = 1.0e+30;
-    b = 9.0e+38;
+    b = 9.0e+38; // > 3.0e+38 => INF
     c = a * b;
-    d = a / b;
+    d = a / b;  // Divide by INF
     e = 1.1e-9 + d;
 
     printf("c value is: %e\n",c);
